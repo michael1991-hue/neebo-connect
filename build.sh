b485fcfac8f50673c1135e7945324cfda5f99eba
michael1991-hue <michael1991@live.co.uk>
2026-09-13 15:21:14 +0100

    Link frameworks required by Nivvi interface and notifications

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p "$app_dir"
 sdk_path="$(xcrun --sdk iphoneos --show-sdk-path)"
 xcrun swiftc -swift-version 5 -parse-as-library -O \
   -sdk "$sdk_path" -target arm64-apple-ios16.0 \
-  -module-name NeeboConnect -framework SwiftUI -framework CoreBluetooth \
+  -module-name NeeboConnect -framework SwiftUI -framework CoreBluetooth -framework Charts -framework AudioToolbox -framework UserNotifications \
   -Xlinker -rpath -Xlinker @executable_path/Frameworks \
   NeeboConnect.swift -o "$app_dir/NeeboConnect"
 cat > "$app_dir/Info.plist" <<'PLIST'
